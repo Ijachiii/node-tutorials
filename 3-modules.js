10c2d0287fe55246612c8700a4c376c30e151437
David Audu <davidaudu1010@gmail.com>
2023-09-28 00:24:54 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules 
+const names = require("./4-names.js")
+const sayHi = require("./5-utils.js")
+const data = require("./6-alternative")
+require("./7-mind-grenade")
+
+console.log(data)
+
+sayHi("Susan")
+sayHi(names["john"])
+sayHi(names["peter"])
---
+++ 6-alternative.js
@@ -0,0 +1,6 @@
+module.exports.items = ["item1", "item2"];
+const person = {
+    name: "Ijachi"
+}
+
+module.exports.singlePerson = person
